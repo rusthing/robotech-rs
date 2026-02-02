6db4d7a0326e912bcddadf821c2ba7e530a45c94
zbz <nnzbz@qq.com>
2026-02-02 11:51:21 +0800

    * 支持发送信号启动和重启程序

--- src/args/pid_utils.rs
+++ src/args/pid_utils.rs
@@ -1,17 +1,33 @@
 use crate::env::{Env, ENV};
 use libc::pid_t;
+use log::info;
 use std::fs::File;
 use std::io::{BufRead, BufReader, BufWriter, Write};
+use std::path::PathBuf;
 use std::process;
 
-pub fn read_pid() -> Option<pid_t> {
+pub struct PidFileGuard;
+
+impl Drop for PidFileGuard {
+    fn drop(&mut self) {
+        delete_pid();
+    }
+}
+
+fn get_pid_file_path() -> PathBuf {
     let Env { app_file_path, .. } = ENV.get().expect("ENV is None");
-    let mut pid_file = app_file_path.clone();
-    pid_file.add_extension("pid");
-    if !pid_file.exists() {
+    let mut pid_file_path = app_file_path.clone();
+    pid_file_path.add_extension("pid");
+    pid_file_path
+}
+
+pub fn read_pid() -> Option<pid_t> {
+    info!("Reading PID...");
+    let pid_file_path = get_pid_file_path();
+    if !pid_file_path.exists() {
         return None;
     }
-    let pid_file = pid_file.to_str().unwrap();
+    let pid_file = pid_file_path.to_str().unwrap();
     let pid_file = File::open(pid_file).unwrap();
     let reader = BufReader::new(pid_file);
     let mut lines = reader.lines();
@@ -26,13 +42,31 @@ pub fn read_pid() -> Option<pid_t> {
     )
 }
 
-pub fn write_pid() {
-    let Env { app_file_path, .. } = ENV.get().expect("ENV is None");
-    let pid = process::id();
-    let mut pid_file = app_file_path.clone();
-    pid_file.add_extension("pid");
-    let pid_file = pid_file.to_str().unwrap();
+pub fn write_pid() -> PidFileGuard {
+    info!("Writing PID...");
+    let pid_file_path = get_pid_file_path();
+    let pid_file_guard = create_pid_file_guard();
+    let pid_file = pid_file_path.to_str().unwrap();
     let pid_file = File::create(pid_file).unwrap();
     let mut writer = BufWriter::new(pid_file);
-    writer.write_all(pid.to_string().as_bytes()).unwrap();
+    writer
+        .write_all(process::id().to_string().as_bytes())
+        .unwrap();
+    pid_file_guard
+}
+
+fn delete_pid() {
+    let pid_option = read_pid();
+    // 如果 PID 文件存在且是当前进程创建的，则删除
+    if let Some(pid) = pid_option
+        && pid == process::id() as pid_t
+    {
+        let pid_file_path = get_pid_file_path();
+        info!("Deleting {pid_file_path:?} ...");
+        std::fs::remove_file(pid_file_path).unwrap();
+    }
+}
+
+fn create_pid_file_guard() -> PidFileGuard {
+    PidFileGuard {}
 }
--- src/args/signal_utils.rs
+++ src/args/signal_utils.rs
@@ -1,28 +1,45 @@
-use crate::args::pid_utils::{read_pid, write_pid};
+use crate::args::pid_utils::{read_pid, write_pid, PidFileGuard};
 use log::{error, info};
-use nix::sys::signal;
+use nix::sys::signal::kill;
+use nix::sys::signal::Signal;
 use nix::unistd::Pid;
 use std::process;
-use tokio::signal::unix::{signal, Signal, SignalKind};
 
-pub fn parse_and_handle_signal_args(signal: Option<String>) {
+pub fn parse_and_handle_signal_args(signal: Option<String>) -> PidFileGuard {
     info!("Signal args: {:?}", signal);
-    if let Some(signal) = signal {
-        let _ = send_signal(&signal).map_err(|e| {
+    let pid_option = read_pid();
+    if let Some(signal) = signal
+        && signal != "start"
+    {
+        let _ = send_signal(&signal, &pid_option).map_err(|e| {
             error!("Failed to send signal: {}", e);
             process::exit(1);
         });
+    } else {
+        // 没有信号参数或者信号参数为start，如果存在PID文件且进程存在(发送信号 0 来检查进程是否存在)，则提示并退出
+        if let Some(pid) = pid_option
+            && kill(Pid::from_raw(pid), Signal::SIGCONT).is_ok()
+        {
+            panic!(
+                "PID file already exists, please confirm whether the program is running. If not, please delete the PID file before running the program again."
+            );
+        };
     }
-    write_pid();
+    write_pid()
 }
 
-fn send_signal(signal_str: &str) -> std::io::Result<Signal> {
-    let pid = read_pid().expect("Failed to read pid");
+fn send_signal(signal_str: &str, pid_option: &Option<i32>) -> std::io::Result<()> {
+    let pid = pid_option.expect("Failed to read pid");
     let signal_str = signal_str.to_lowercase();
     match signal_str.as_str() {
-        "reload" | "r" => signal::kill(Pid::from_raw(pid), signal::Signal::SIGHUP).expect("SIGHUP"),
-        "quit" | "q" => signal::kill(Pid::from_raw(pid), signal::Signal::SIGINT).expect("SIGINT"),
-        "stop" | "s" => signal::kill(Pid::from_raw(pid), signal::Signal::SIGTERM).expect("SIGTERM"),
+        "restart" => {
+            let _ = kill(Pid::from_raw(pid), Signal::SIGINT);
+            return Ok(());
+        }
+        "load" | "reload" | "l" => kill(Pid::from_raw(pid), Signal::SIGHUP).expect("SIGHUP"),
+        "quit" | "q" => kill(Pid::from_raw(pid), Signal::SIGINT).expect("SIGINT"),
+        "stop" | "s" => kill(Pid::from_raw(pid), Signal::SIGTERM).expect("SIGTERM"),
+        "kill" | "k" => kill(Pid::from_raw(pid), Signal::SIGKILL).expect("SIGKILL"),
         _ => panic!("Invalid signal({signal_str})"),
     };
     process::exit(0);
