873de1b5d2ef85741ddab04310f53d134a32c6cb
nnzbz <nnzbz@qq.com>
2026-08-23 13:42:27 +0800

    * db_conn_utils使用ArcSwap机制

--- robotech/src/db/db_utils.rs
+++ robotech/src/db/db_utils.rs
@@ -1,45 +1,35 @@
 use crate::db::{DbConnConfig, DbError};
-use robotech_macros::log_call;
+use arc_swap::ArcSwapOption;
+use config::Value;
 use sea_orm::{ConnectOptions, Database, DbConn};
-use std::sync::{Arc, RwLock};
-use tracing::debug;
+use std::collections::HashMap;
+use std::sync::Arc;
+use tracing::info;
 
 /// 数据库连接
-static DB_CONN: RwLock<Option<Arc<DbConn>>> = RwLock::new(None);
+static DB_CONN: ArcSwapOption<DbConn> = ArcSwapOption::const_empty();
 
-/// 获取App配置的只读访问
+/// 获取数据库连接的只读访问
 pub fn get_db_conn() -> Result<Arc<DbConn>, DbError> {
-    let read_lock = DB_CONN.read().map_err(|_| DbError::GetDbConn())?;
-    read_lock.clone().ok_or(DbError::GetDbConn())
+    DB_CONN.load_full().ok_or(DbError::GetDbConn())
 }
 
-/// 设置App配置
-pub fn set_db_conn(value: DbConn) -> Result<(), DbError> {
-    let mut write_lock = DB_CONN.write().map_err(|_| DbError::SetDbConn())?;
-    *write_lock = Some(Arc::new(value));
+pub async fn setup_db_conn(
+    db_conn_config: DbConnConfig,
+    changed: &Option<HashMap<String, Value>>,
+) -> Result<(), DbError> {
+    info!("setup db connection...");
+    if changed
+        .as_ref()
+        .map(|changed| changed.contains_key("db"))
+        .unwrap_or(true)
+    {
+        // 获取数据库配置
+        let opt: ConnectOptions = db_conn_config.into();
+        // 连接数据库
+        let connection = Database::connect(opt).await.map_err(DbError::Connect)?;
+        // 设置数据库连接到全局变量中
+        DB_CONN.store(Some(Arc::new(connection)));
+    }
     Ok(())
-}
-
-/// # 初始化数据库连接
-///
-/// 该函数接收数据库配置信息，建立数据库连接，并将连接存储到全局静态变量 `DB_CONN` 中。
-/// 连接建立后，可以通过 `DB_CONN` 全局访问数据库连接。
-///
-/// # 参数
-///
-/// * `db_config` - 数据库配置信息，包含连接数据库所需的信息
-///
-/// # Panics
-///
-/// * 如果数据库连接失败，程序将 panic
-/// * 如果无法设置全局数据库连接，程序将 panic
-#[log_call]
-pub async fn init_db_conn(db_conn_config: DbConnConfig) -> Result<(), DbError> {
-    debug!("init database...");
-    // 获取数据库配置
-    let opt: ConnectOptions = db_conn_config.into();
-    // 连接数据库
-    let connection = Database::connect(opt).await.map_err(DbError::Connect)?;
-    // 设置数据库连接到全局变量中
-    set_db_conn(connection)
 }
